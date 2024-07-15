ffad5c81a60598a2a16766a18d60e289c7a2ae6d
Hans <hans.doenitz@dasei.eu>
2024-07-15 07:04:19 +0200

    integrated tailwind-typography

--- pages/typography.vue
+++ pages/typography.vue
@@ -1,6 +1,6 @@
 <template>
   <LayoutHeader />
-  <div class="container px-4 md:px-8 py-6">
+  <div class="container max-w-[640px] ml-10 px-4 md:px-8 py-6">
     <h1>
       <span style="float: left; font-size: 60px; line-height: 0.85em; font-weight: bold; margin-right: 7px;">
         A1
@@ -24,6 +24,7 @@
     </div>
 
     <div class="mt-4">
+      <pre>First Draft</pre>
       <h1>
         <span style="float: left; line-height: 0.85em;" class="text-6xl pr-2">A1
         </span>
@@ -33,13 +34,211 @@
       </h1>
     </div>
 
-    <div>
+    <div class="mt-4">
+      <pre>A: H2 default (Overline)
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        Szenische Themenarbeit mit kreativen Bildern
+      </h2>
       <p>
-        ein in 2022-2023 umgesetzter Design-Prozess hat die verschiedensten Anforderungen und Use-Cases ermittelt und dazu mehrere Design-Vorlagen entwickelt
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
       </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>B: H2 strong
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <strong>Klischees, Situationen & Skulpturen</strong>
+      </h2>
       <p>
-        ausgehend von Javascript-Component-Frameworks und Obsidian-Themes werden diese Design-Vorlagen weiterentwickelt und angepasst mit dem Ziel einer hohen technischen wie inhaltlichen Qualität
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
       </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>C: H2 display
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <em>Klischees, Situationen & Skulpturen</em>
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>D: H2 Overline-Headline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        Szenische Themenarbeit mit kreativen Bildern
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>E: H2 Headline-Subline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <strong>Klischees, Situationen & Skulpturen</strong>
+        Szenische Themenarbeit mit kreativen Bildern
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>F: H2 Shortcode-Overline-Headline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <em>B2</em>
+        Szenische Themenarbeit mit kreativen Bildern
+        <strong>Klischees, Situationen & Skulpturen</strong>
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>G: H2 Shortcode-Headline-Subline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <em>B2</em>
+        <strong>Klischees, Situationen & Skulpturen</strong>
+        Szenische Themenarbeit mit kreativen Bildern
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>H: H2 Tag-Headline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <mark class="font-mona">17.-19.1 (Nbg)</mark>
+        <strong>Klischees, Situationen & Skulpturen</strong>
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>I: H2 Shortcode-Tag-Overline-Headline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <em>B2</em>
+        <mark class="font-mona">17.-19.1 (Nbg)</mark>
+        Szenische Themenarbeit mit kreativen Bildern
+        <strong>Klischees, Situationen & Skulpturen</strong>
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>J: H2 Shortcode-Headline-Tag-Subline
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px / MARGIN-BOTTOM: mb-0.5
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <em>B2</em>
+        <strong class="mb-0.5">Klischees, Situationen & Skulpturen</strong>
+        <mark class="font-mona">17.-19.1 (Nbg)</mark>
+        Szenische Themenarbeit mit kreativen Bildern
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <pre>XX: H2 TEMPLATE
+  - SC: 60px / Line-Height: 0.9em
+  - HL: 24px
+  - OL: 18px
+  - TAG: 14px / Line-Height: 12px
+      </pre>
+      <h2 class="mb-2">
+        <em>B2</em>
+        <mark class="font-mona">17.-19.1 (Nbg)</mark>
+        Szenische Themenarbeit mit kreativen Bildern
+        <strong>Klischees, Situationen & Skulpturen</strong>
+      </h2>
+      <p>
+        Wir lernen, Einstiege und Ausstiege mit spielerischen Standbildern zu gestalten, innere Haltungen, Veränderungen und Ergebnisse sichtbar zu machen.
+      </p>
+    </div>
+
+    <div class="mt-96">
+      <pre>Fill-Content
+      </pre>
+
+      <h1>
+        <span style="float: left; font-size: 60px; line-height: 0.85em; font-weight: bold; margin-right: 7px;">
+          A1
+        </span>
+        <span class="text-base">
+          Einführung in die Kreisanimation
+        </span>
+        <br>
+        <span style="padding-bottom: 10px;" class="text-2xl">
+          Am Anfang war der Kreis
+        </span>
+      </h1>
+
+      <div>
+        <p>
+          ein in 2022-2023 umgesetzter Design-Prozess hat die verschiedensten Anforderungen und Use-Cases ermittelt und dazu mehrere Design-Vorlagen entwickelt
+        </p>
+        <p>
+          ausgehend von Javascript-Component-Frameworks und Obsidian-Themes werden diese Design-Vorlagen weiterentwickelt und angepasst mit dem Ziel einer hohen technischen wie inhaltlichen Qualität
+        </p>
+      </div>
     </div>
   </div>
   <LayoutFooter />
@@ -48,3 +247,91 @@
 <script setup lang="ts">
 
 </script>
+
+<style scoped>
+/* -----------------------------
+-----   H1-Formatting ---------*/
+
+pre {
+  font-size: 12px;
+  margin-left: -40px;
+}
+
+h1 > strong {
+  font-size: 30px;
+}
+h1:has(em) > strong {
+  font-size: 40px !important;
+}
+h1 > strong {
+  display: block;
+  font-style: normal;
+  line-height: 1em;
+  text-transform: uppercase;
+  font-weight: 700;
+}
+
+h1 > mark {
+  font-size: 14px;
+  margin-right: 4px;
+  float: block-start;
+  line-height: 14px;
+  padding-left: 4px;
+  padding-right: 4px;
+  background-color: grey;
+  font-weight: 900;
+}
+
+/* -----------------------------
+-----   H2-Formatting ---------*/
+
+h2 {
+  font-size: 18px;
+}
+
+h2 > strong {
+  font-size: 24px !important;
+  display: block;
+  font-style: normal;
+  line-height: 1.05em;
+  text-transform: uppercase;
+  font-weight: 700;
+}
+
+/* Shortcode */
+h2 > em {
+  line-height: 0.9em;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 60px !important;
+  float: left;
+  margin-right: 10px;
+}
+
+h2 > mark{
+  font-size: 14px;
+  margin-right: 4px;
+  float: block-start;
+  line-height: 12px;
+  padding-left: 4px;
+  padding-right: 4px;
+  text-transform: uppercase;
+  background-color: grey;
+  font-weight: 600;
+}
+
+/* Card-Display-Style: gelbes Band hinter Subline */
+/* entsteht via Card-H2 */
+
+/* Card-default: gelbes Band */
+/* entsteht via Card-H3 */
+
+/* H3-Headline-Formatting / Card: gelbes Band
+.card-display :is(.HyperMD-header-2, h2):has(strong, .cm-strong) > :is(strong, .cm-strong) {
+  font-size: 18px !important;
+  margin-left: 50px;
+  padding-left: 4px;
+  background-color: gold;
+  line-height: 16px;
+} */
+</style>
