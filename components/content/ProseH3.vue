37be78b2944846e7363e16f8b10cf76ff4d54bbc
Hans <hans.doenitz@dasei.eu>
2024-07-16 10:40:54 +0200

    H3 overline-headline-shortcode for mobile>notebook-screens

--- components/content/ProseH3.vue
+++ components/content/ProseH3.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3 :id="id" class="scroll-m-20 text-xl lg:text-2xl font-semibold tracking-tight [&:not(:first-child)]:mt-8">
+  <h3 :id="id" class="scroll-m-20 [&:not(:first-child)]:mt-8" :class="noprose ? 'text-xl lg:text-2xl font-semibold tracking-tight ' : ''">
     <NuxtLink
       v-if="id && generate"
       :href="`#${id}`"
@@ -11,7 +11,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ id?: string }>();
+const props = defineProps<{ id?: string; noprose?: boolean }>();
 
 const { headings } = useRuntimeConfig().public.mdc;
 const generate = computed(() => props.id && headings?.anchorLinks?.h3);
