5d03f38dcbbfe1c1f249607d986f358d49ee4c79
Hans <hans.doenitz@dasei.eu>
2024-07-18 22:06:33 +0200

    temporary hack for blockquote >needed to add sections h2-tags

--- components/content/ProseBlockquote.vue
+++ components/content/ProseBlockquote.vue
@@ -1,5 +1,10 @@
 <template>
+  <!-- HACK: This is a temporary solution to speed up development.
   <blockquote class="mt-6 border-l-2 pl-6 italic">
     <slot />
-  </blockquote>
+  </ blockquote -->
+  <section class="mt-6">
+    <slot />
+  </section>
+
 </template>
